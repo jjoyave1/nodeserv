0dc1229954df846b444f27c3c87cb185daab0c0a
Joe Joyave <jjoyave1@gmail.com>
2015-08-05 00:03:14 -0400

    server working with basic json data

---
+++ app/js/server.js
@@ -0,0 +1,42 @@
+// BASE SETUP
+// ----------------------------------------
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// bodyParser() configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080; // SET PORT (changeable)
+
+// ----------------------------------------
+
+
+// API ROUTES
+// ----------------------------------------
+
+var router = express.Router(); // get instance of express Router
+
+router.get('/', function ( req, res ) {
+  res.json({ message: 'WELCOME TO THE API' });
+});
+
+// add in routes here
+
+// ----------------------------------------
+
+
+// REGISTER ROUTES
+// ----------------------------------------
+
+app.use('/api', router);
+
+// ----------------------------------------
+
+
+// START THE SERVER
+// ----------------------------------------
+app.listen(port);
+console.log("listening on port " + port);
